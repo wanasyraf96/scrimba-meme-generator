665c1d9ed2729b16ec50626101b23005c9038d81
WaNa <wanasyraf96@yahoo.com>
2023-06-18 19:28:03 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import alias from "@rollup/plugin-alias";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    alias({
+      entries: [
+        { find: "@styles", replacement: "./src/styles" },
+        { find: "@components", replacement: "./src/components" },
+      ],
+    }),
+  ],
+  server: {
+    host: true,
+    port: 8000,
+    watch: {
+      usePolling: true,
+    },
+  },
+});
